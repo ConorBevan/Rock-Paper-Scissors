2d783af03917af8d0b3a4278fdc78dca150d344a
ConorBevan <conor.bevan@hotmail.com>
2022-10-31 12:17:40 +0000

    reorganised the layout of my code in html and js file to be more chronological in order of events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+/* Login Form taken from https://medium.com/swlh/how-to-create-your-first-login-page-with-html-css-and-javascript-602dd71144f1 */
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+const parentContainer =  document.getElementById("parent-container")
+const startContainer = document.getElementById("start-container")
+
+/* When you click play game button it will not take you to the game unless you put in a value in the input */
+loginButton.addEventListener("click", (e) => {
+    "use strict";
+    e.preventDefault();
+    const username = loginForm.username.value;
+
+    if (username.trim() == "") {
+        alert("Enter a username.");
+    } else {
+        parentContainer.style.display = 'block';
+        startContainer.style.display = 'none'
+    }
+});
+
 var rock = document.getElementById('rock');
 var paper = document.getElementById('paper');
 var scissors = document.getElementById('scissors');
@@ -69,29 +90,8 @@ resetBtn.addEventListener("click", () => {
     compscore.innerText = 0;
 });
 
-/* when you click this button an alert is prompted to display game rules */
+/* When you click this button an alert is prompted to display game rules */
 rulesBtn.addEventListener("click", () => {
     "use strict";
     alert('Rock wins against scissors; paper wins against rock; and scissors wins against paper. If both players throw the same hand signal, it is considered a tie, and play resumes. Good luck!');
-});
-
-/* Login Form taken from https://medium.com/swlh/how-to-create-your-first-login-page-with-html-css-and-javascript-602dd71144f1 */
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-
-const parentContainer =  document.getElementById("parent-container")
-const startContainer = document.getElementById("start-container")
-
-/* When you click play game button it will not take you to the game unless you put in a value in the input */
-loginButton.addEventListener("click", (e) => {
-    "use strict";
-    e.preventDefault();
-    const username = loginForm.username.value;
-
-    if (username.trim() == "") {
-        alert("Enter a username.");
-    } else {
-        parentContainer.style.display = 'block';
-        startContainer.style.display = 'none'
-    }
 });
