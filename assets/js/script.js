6ea209023753672f832923bf0e1b06c6674e8622
ConorBevan <conor.bevan@hotmail.com>
2022-10-31 12:14:20 +0000

    added all code to one html page and moved js code to one file also

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,24 @@ rulesBtn.addEventListener("click", () => {
     "use strict";
     alert('Rock wins against scissors; paper wins against rock; and scissors wins against paper. If both players throw the same hand signal, it is considered a tie, and play resumes. Good luck!');
 });
+
+/* Login Form taken from https://medium.com/swlh/how-to-create-your-first-login-page-with-html-css-and-javascript-602dd71144f1 */
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+const parentContainer =  document.getElementById("parent-container")
+const startContainer = document.getElementById("start-container")
+
+/* When you click play game button it will not take you to the game unless you put in a value in the input */
+loginButton.addEventListener("click", (e) => {
+    "use strict";
+    e.preventDefault();
+    const username = loginForm.username.value;
+
+    if (username.trim() == "") {
+        alert("Enter a username.");
+    } else {
+        parentContainer.style.display = 'block';
+        startContainer.style.display = 'none'
+    }
+});
